9266f3bede824178d230ba1f643f3725d10cfc91
Yogesh <yogeshkr210@gmail.com>
2022-10-11 17:39:40 +0530

    getting product from API

--- App.js
+++ App.js
@@ -1,17 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [products, setProducts] = useState([])
+
+  const fetchProducts = async () => {
+    const res = await fetch('https://fakestoreapi.com/products', { method: "GET" })
+    const data = await res.json();
+    console.log(data);
+    setProducts(data);
+  }
+
+  const product = (item) => {
+    return (
+      <View key={item.id} style={{flex:1}}>
+        <View></View>
+        <View></View>
+        <Text>hello</Text>
+      </View>
+    )
+  }
+
+  useEffect(() => {
+    fetchProducts()
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <View style={styles.main}>
+        {products.map(product)}
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  main: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
